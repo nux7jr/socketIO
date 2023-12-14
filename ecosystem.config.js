f506ad874b9c40329dd749c16861cdd0abacc1ae
nux7jr <nux7jr@gmail.com>
2023-12-14 22:38:46 +0700

    add css

---
+++ ecosystem.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    apps: [
+        {
+            name: 'chat',
+            port: '3000',
+            script: './app.js',
+        }
+    ],
+    deploy: {
+        production: {
+            user: 'root',
+            host: ['213.110.228.1'],
+            ref: 'origin/main',
+            repo: 'git@github.com:nux7jr/socketIO.git',
+            ssh_options: ['ForwardAgent=yes'],
+            path: '/var/www/socketIO',
+            "pre-deploy": "git fetch --all",
+            'post-deploy': 'yarn && yarn build && pm2 startOrRestart ecosystem.config.js --env production setup'
+        }
+    }
+}
---
+++ public/js/app.js
@@ -0,0 +1,25 @@
+var socket = io();
+
+var messages = document.querySelector('.messages');
+var form = document.querySelector('.form');
+var input = document.querySelector('.input');
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  if (input.value) {
+    socket.emit('chat message', input.value);
+    input.value = '';
+  }
+});
+
+socket.on('chat message', function (msg) {
+  var item = document.createElement('li');
+  item.textContent = msg;
+  messages.appendChild(item);
+  window.scrollTo(0, document.body.scrollHeight);
+});
+
+document.querySelector('.input-file input[type=file]').on('change', function () {
+  let file = this.files[0];
+  document.querySelector(this).next().html(file.name);
+});
