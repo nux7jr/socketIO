a3806c9b09d9fd0fa23c4fe821e9ec3da315bd10
nux7jr <nux7jr@gmail.com>
2023-12-16 20:14:51 +0700

    trash slade

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -21,9 +21,9 @@ module.exports = {
             ref: 'origin/main',
             repo: 'git@github.com:nux7jr/socketIO.git',
             ssh_options: ['ForwardAgent=yes'],
-            path: '/home/mike/projects/socketIO',
+            path: '/var/www/socketIO',
             'pre-deploy-local': 'yarn',
-            'post-deploy': 'cd /home/mike/projects/socketIO && git fetch --all && yarn && yarn build && pm2 startOrRestart ecosystem.config.js --env production',
+            'post-deploy': 'git fetch --all && yarn && yarn build && pm2 startOrRestart ecosystem.config.js --env production',
         }
     }
 }
