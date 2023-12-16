7e4394336c29c40c38c593f9463dd17d81b83372
nux7jr <nux7jr@gmail.com>
2023-12-16 20:59:00 +0700

    nyje

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,8 +5,6 @@ module.exports = {
             port: '3000',
             script: './app.js',
             max_memory_restart: "300M",
-
-
             out_file: "./logs/out.log",
             error_file: "./logs/error.log",
             merge_logs: true,
