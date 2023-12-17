ee96b95469a47eacc3c64622ed3ca4dca29fc163
nux7jr <nux7jr@gmail.com>
2023-12-17 11:55:17 +0700

    root

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -14,7 +14,7 @@ module.exports = {
     ],
     deploy: {
         production: {
-            user: 'pm2',
+            user: 'root',
             host: ['213.110.228.1'],
             ref: 'origin/main',
             repo: 'git@github.com:nux7jr/socketIO.git',
